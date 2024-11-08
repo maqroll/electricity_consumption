263204963b97fc43aedcc192d34b04b7ce93840c
maqroll <loteroc@gmail.com>
2024-11-08 17:16:04 +0100

    Giving up on naturgy scraping. Going for rust. Keep this one just for post-processing.

--- src/main/java/scrape/App.java
+++ src/main/java/scrape/App.java
@@ -1,9 +1,9 @@
 package scrape;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
@@ -16,25 +16,25 @@ public class App {
     try {
       run(args);
     } catch (IOException | InvalidFormatException e) {
-      log.error("Scrape failed.",e);
+      log.error("Scrape failed.", e);
       System.exit(1);
     }
   }
 
   public static void run(String[] args) throws IOException, InvalidFormatException {
-    if (args.length < 3) {
-      throw new IllegalArgumentException(
-          "Missing arguments (user and/or password and/or filename)");
+    if (args.length < 2) {
+      throw new IllegalArgumentException("Missing arguments: filename path...");
     }
 
-    CSV data = new CSV(args[2]);
+    CSV data = new CSV(args[0]);
     String maxDate = data.maxValue(0);
 
-    Scrape scrape = new Scrape(args[0] /* user */, args[1] /* password */);
-    scrape.searchInvoicesAfter(LocalDate.parse(maxDate, DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+    for (int i = 1; i < args.length; i++) {
+      Path path = Paths.get(args[i]);
+      byte[] xls = Files.readAllBytes(path);
 
-    while (scrape.hasMoreInvoices()) {
-      CSV csv = scrape.nextInvoice();
+      CSV csv = CSVUtils.convertExcelToCSV(xls);
+
       csv.getRidOfColumn(2); // TO column
       csv.transformColumn(
           0,
@@ -61,7 +61,7 @@ public class App {
             }
             return res;
           });
-        csv.appendTo(args[2]);
+      csv.appendTo(args[0]);
     }
   }
 }
